803167fe6d607b8b046701c4e5a2915c796e30b6
Uuuuuumm <tateo9119@gmail.com>
2022-03-14 17:12:34 -0400

    very basic template created; sample data added

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy
+
+
+class game:
+    def __init__(self):
+        pass
+
+
+
+def main():
+    print("Hello World")
+    with open('sampleData.pgn') as f:
+        for line in f:
+            pass
+
+
+main()
